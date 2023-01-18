e9a08f61fc4d5fdd52e63c288ddb72e171db120c
janne <janne.koponen@savonia.fi>
2023-01-18 10:08:49 +0200

    Initial version

---
+++ 02/02_main.cs
@@ -0,0 +1,13 @@
+﻿namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give your name");
+            String name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
---
+++ 03/03_main.cs
@@ -0,0 +1,28 @@
+﻿namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give first string : ");
+            string firstString = Console.ReadLine();
+
+            Console.Write("Give second string : ");
+            string secondString = Console.ReadLine();
+
+            if (String.Compare(firstString, secondString) == 0)
+            {
+                Console.WriteLine("The strings are equal : " + firstString);
+            }
+            else if (String.Compare(firstString, secondString) < 0)
+            {
+                Console.WriteLine("The first in alhabetical order is " + firstString);
+            }
+            else
+            {
+                Console.WriteLine("The first string in alphabetical order is "
+                    + secondString);
+            }
+        }
+    }
+}
