96937481da824b0f0e46674a46b8b6127a9bea75
janne <janne.koponen@savonia.fi>
2023-02-08 13:29:51 +0200

    14-19 added

---
+++ 14/14_main.cs
@@ -0,0 +1,83 @@
+﻿//Contains solution of 14...19
+
+namespace _14
+{
+    class Car
+    {
+        public List<Car> cars = new List<Car>();
+        public static int counter = 0;
+        public decimal odometer = 0;
+        public void Drive(decimal kms)
+        {
+            if (kms <= 0)
+            {
+                throw new ArgumentOutOfRangeException("kms<0");
+            }
+            odometer += kms;
+        }
+        public Car()
+        {
+            counter++;
+            cars.Add(this);
+        }
+
+        static public void DriveAll(decimal kms)
+        {
+            foreach (Car car in cars)
+            {
+                car.Drive(kms);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+
+            Console.WriteLine(car.odometer);
+            car.Drive(10);
+            Console.WriteLine(car.odometer);
+
+            try
+            {
+                car.Drive(-10);
+            } catch {
+                Console.WriteLine("Got an exception");
+            }
+
+            const int N = 10;
+
+            var carlist=new List<Car>();
+
+            for(int i=0; i<N; i++)
+            {
+                carlist.Add(new Car());
+            }
+
+            foreach(var c in carlist)
+            {
+                c.Drive(1000);
+            }
+
+            var rnd=new Random();   
+
+            foreach (var c in carlist)
+            {
+                try
+                {
+                    c.Drive(rnd.Next(-10, 501));
+                }
+                catch
+                {
+                    Console.WriteLine("Got an exception here!");
+                }
+            }
+
+
+            Console.WriteLine(Car.counter);
+
+            Car.DriveAll(10);
+        }
+    }
+}
---
+++ temp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        /*       static void Main(string[] args)
+               {
+                   string userInput = Console.ReadLine();
+                   int counter = 0;
+                   string[] strings = new string[100];
+
+                   while (userInput != "END")
+                   {
+                       strings[counter] = userInput;
+                       userInput = Console.ReadLine();
+                       counter++;
+                   }
+
+                   for (int i = 0; i < counter; i = i + 2)
+                   {
+                       Console.WriteLine(strings[i]);
+                   }
+               }*/
+
+        public class Car
+        {
+            private decimal odometer;
+            public decimal Odometer
+            {
+                get { return odometer; }
+                private set { odometer = value; }
+            }
+
+            public void Drive(decimal kms)
+            {
+                if (kms <= 0)
+                {
+                    throw new ArgumentException("Distance must be greater than 0");
+                }
+                odometer += kms;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            List<Car> carList = new List<Car>();
+            for (int i = 0; i < 10; i++)
+            {
+                Car car = new Car();
+                car.Drive(1000);
+                carList.Add(car);
+            }
+
+
+
+        }
+
+    }
+}
+
+
